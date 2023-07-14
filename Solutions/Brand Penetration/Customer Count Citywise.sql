416b28edc73f6b8e590798fb5d04b47d12035ef6
Pranjal Barve <90856333+perkypranjal@users.noreply.github.com>
2023-07-14 17:54:43 +0530

    Update Customer Count Citywise.sql

--- Solutions/Brand Penetration/Customer Count Citywise.sql
+++ Solutions/Brand Penetration/Customer Count Citywise.sql
@@ -1,1 +1,8 @@
+# Which cities do we need to focus more on?
 
+select c.city, count(sr.respondent_id) as customer_count
+from dim_cities c join fact_survey_responses_cityid sr
+using (city_id)
+where sr.current_brands like"%Codex"
+group by c.city
+order by count(sr.respondent_id) desc
