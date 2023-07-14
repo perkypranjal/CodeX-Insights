c930d763e1e29892107923cff325d7c55aabb6ca
Pranjal Barve <90856333+perkypranjal@users.noreply.github.com>
2023-07-14 17:55:41 +0530

    Update What people think about codex.sql

--- Solutions/Brand Penetration/What people think about codex.sql
+++ Solutions/Brand Penetration/What people think about codex.sql
@@ -1,1 +1,11 @@
+# What do people think about our brand? (overall rating)
 
+select 
+Current_brands, 
+Brand_perception , 
+count(Brand_perception) As feedback_count , 
+round(100*count(Brand_perception)/ sum(count(Brand_perception)) over (),2) as "%feedback_count"
+from fact_survey_responses
+where current_brands = "Codex"
+group by Brand_perception 
+order by feedback_count desc
